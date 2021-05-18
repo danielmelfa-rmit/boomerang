3aa1ee3c4f45f9d9c435ae7b5afc0cf255e8af3b
danielmelfa-rmit <80460036+danielmelfa-rmit@users.noreply.github.com>
2021-05-18 21:31:22 +1000

    commit3 - some scripts renamed
    commit3 - some scripts renamed

---
+++ Daniel - MySql/insert into users.sql
@@ -0,0 +1,4 @@
+insert into boomerangdb.users (email,lastName,firstName,street,suburb,state,postCode,userStatus,userType)
+values ('rom65@hotmail.com','Romulus','Igor','5 Old South Road','Frankton','VIC','3112','Active','Lessor-Renter')
+
+-- comment
---
+++ Daniel - MySql/insert items.sql
@@ -0,0 +1,2 @@
+insert into boomerangdb.items (itemName,itemDescription,itemPrice,itemStatus,itemDateAdded,itemImagePath,userID)
+values ('Old Tractor','Oldie but goodie tractor','25.50','Available',20210518,Null,1)
